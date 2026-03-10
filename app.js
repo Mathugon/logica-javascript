8f0453ab114ff94869a7cafa8fc36c702f1f6444
Matheus Gontijo <matheus.silva.gontijo@gmail.com>
2026-03-10 17:19:24 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 alert('Boas-vindas ao meu jogo do número secreto!');
+let numeroSecreto  = 29; 
+let tentativa = prompt('Digite um número entre 1 e 50');
+
+if (tentativa == numeroSecreto) {
+    console.log('Parabéns! Você adivinhou o número secreto!');
+
+}
